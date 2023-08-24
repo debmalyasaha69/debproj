5ee19828d271e0bf667cd4a8d174908be8a6bcdb
debmalyasaha69 <142111592+debmalyasaha69@users.noreply.github.com>
2023-08-24 20:57:50 +0530

    Add files via upload

---
+++ numberguess.py
@@ -0,0 +1,22 @@
+import random
+
+max=100
+min=1
+randomnum=random.randint(min,max)
+attempts=0
+guessedcorrectly= False
+while not guessedcorrectly:
+    userguess=int(input("Enter your number:"))
+    if userguess<=max:
+      attempts+=1
+      if userguess<randomnum:
+        print("Low! try a higher number")
+      elif userguess>randomnum:
+        print("High! try a low number")
+      else:
+        guessedcorrectly= True
+        print("Congratulations! you got it right")
+        print("You guessed the number ",randomnum," in ",attempts," attempts")
+    else:
+        print("Error")
+        break
